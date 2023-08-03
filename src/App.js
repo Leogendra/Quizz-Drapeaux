b524dad62c2791d7a104ca25263022ca57c24eb4
Gatien <le.magicien.annonyme@gmail.com>
2023-08-03 19:14:49 +0200

    Modification des routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, useParams} from 'react-router-dom';
+
 import About from './pages/About';
 import Home from './pages/Home';
 import Cartes from './pages/Cartes';
 
-const App = () => {
+const DynamicPage = () => {
+  const { page } = useParams();
+
+  switch(page.toLowerCase()) {
+    case "home":
+      return <Home />;
+    case "about":
+      return <About />;
+    case "cartes":
+      return <Cartes />;
+    default:
+      return <Home />;
+  }
+}
+
+function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cartes" element={<Cartes />} />
         <Route path="/about" element={<About />} />
-        
+        <Route path="/:page" element={<DynamicPage />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
