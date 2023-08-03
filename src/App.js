f78fe1e05337706dab12e687cc5082f868484b86
Gatien <le.magicien.annonyme@gmail.com>
2023-08-03 19:24:35 +0200

    Clean des routes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
-import {BrowserRouter, Routes, Route, useParams} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import About from './pages/About';
 import Home from './pages/Home';
 import Cartes from './pages/Cartes';
 
-const DynamicPage = () => {
-  const { page } = useParams();
-
-  switch(page.toLowerCase()) {
-    case "home":
-      return <Home />;
-    case "about":
-      return <About />;
-    case "cartes":
-      return <Cartes />;
-    default:
-      return <Home />;
-  }
-}
-
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cartes" element={<Cartes />} />
         <Route path="/about" element={<About />} />
-        <Route path="/:page" element={<DynamicPage />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
 };
-
-export default App;
